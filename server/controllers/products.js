7f201405016a6c9b12c84092788fa1e2664a0d43
manuligit <riina.vaisanen@gmail.com>
2018-03-15 15:17:25 +0200

    Backend and frontend connected, api/post works

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,13 +1,37 @@
 const productsRouter = require('express').Router()
 const Product = require('../models/product')
 
-productsRouter.get('/', (request, response) => {
-  Product
-    .find({})
-    .then(items => {
-      response.json(items)
-    })
+productsRouter.get('/', async (request, response) => {
+  try {
+    const products = await Product.find({})
+    response.json(products.map(Product.format))
+  } catch (exception) {
+    console.log(exception.name)
+    response.status(400).send({ error: 'something went wrong' })
+  }
 })
 
+productsRouter.post('/', async (request, response) => {
+  const body = request.body
+  try {
+    if (body.name === undefined || body.price === undefined) {
+      return response.status(400).json({ error: 'name or price missing' })
+    }
+
+    const product = new Product({
+      name: body.name,
+      category: body.category,
+      price: body.price
+    })
+
+    const savedProduct = await product.save()
+
+    response.json(Product.format(savedProduct))
+  }
+  catch (exception) {
+    console.log('productsRouter error: ', exception.name)
+    response.status(500).json({ error: 'Something went wrong' })
+  }
+}) 
 
 module.exports = productsRouter
--- server/models/product.js
+++ server/models/product.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose')
 
-const Product = mongoose.model('Product', {
+const productSchema = new mongoose.Schema ({
   name: String,
   category: String,
   price: Number
 })
 
+productSchema.statics.format = (product) => {
+  return {
+    id: product.id,
+    name: product.name,
+    category: product.category,
+    price: product.price
+  }
+}
+
+const Product = mongoose.model('Product', productSchema)
 module.exports = Product
